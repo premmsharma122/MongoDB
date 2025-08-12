29bd20da3d24331ced3e895ec908fa3dea98be6e
Prem Sharma <sonusharma4237@gmail.com>
2025-08-13 00:39:27 +0530

    upadte

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const Chat = require('./models/chat.js');
+const path = require('path')
+
+app.set("views", path.join(__dirname,"views"))
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended: true}))
 
 main().catch(err => console.log(err));
 
@@ -9,18 +14,29 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
 
-let chat1 = new Chat({
-    from: "Prem",
-    to: "Sharma",
-    msg: "Hiii",
-    date: new Date()
+app.get('/chats', async (req,res)=>{
+    let chats = await Chat.find()
+    res.render("index.ejs", {chats})
 })
-chat1.save().then((res)=>{
-    console.log(res);
+app.get('/chats/new', (req,res)=>{
+    res.render("new.ejs")
+})
+app.post('/chats', (req,res)=>{
+    let {from, msg, to} = req.body
+    let newChat = new Chat({
+        from: from,
+        msg: msg,
+        to: to,
+        date: new Date()
+    })
+    newChat.save()
+    .then(res => {console.log("Chat is Saved!")})
+    .catch((err)=>{
+        console.log(err);
+    })
     
+    res.redirect("/chats")
 })
-
-
 app.get('/',(req,res)=>{
     res.send("At Root Path")
 })
